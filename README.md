836c68a0cc3ddcdb1d48a37911198e0bb490dad4
gwangmin <ygm.gwangmin@gmail.com>
2020-05-23 01:19:09 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# http_chat
+http_chat is a messenger using http protocol for compatibility
---
+++ main.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, id, pw):
+        pass
+
+
+class Room:
+    def __init__(self, users):
+        self.users = users
+
